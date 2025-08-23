c50fe22b506229e874bb2d261cddabd72124e056
BANK BRI <152972674+brimobile1016@users.noreply.github.com>
2025-08-23 19:32:55 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const { GoogleSpreadsheet } = require('google-spreadsheet');
-const creds = JSON.parse(process.env.GOOGLE_CLOUD_PRIVATE_KEY);
+const creds = require('./creds');
 
  const app = express();
 // const port = 3000;
@@ -151,4 +151,5 @@ app.post('/api/delete', async (req, res) => {
 //});
 
 module.exports = app;
+
 
